fb68c51fa87836a0116367afa28093c100232093
GodwinEgo <godwinehile04@gmail.com>
2023-07-28 03:15:23 +0100

    tzt

--- todo.rb
+++ todo.rb
@@ -1,51 +1,51 @@
-# todo.rb
-require "tty-prompt"
+#todo.rb
 
-# Define the empty array to store tasks
+#defining the empty array to store tasks
 tasks = []
 
-# Method to display all tasks
+#Here, I create an empty array called tasks to store the tasks added by the user.
+#Method to display all tasks
 def display_tasks(tasks)
   puts "\nYour To-Do List:"
   tasks.each_with_index do |task, index|
-    puts "#{index + 1}. #{task}"
+    puts "#{index + 1}.#{task}"
   end
   puts "\n"
 end
 
-# Initialize the prompt
-prompt = TTY::Prompt.new
-
-# Main loop for the application
+#Main loop for the app
 loop do
   puts "Choose an option:"
   puts "1. Add task"
   puts "2. View tasks"
-  puts "3. Mark task as completed"
+  puts "3 Mark task as completed"
   puts "4. Remove task"
   puts "5. Exit"
 
-  choice = prompt.ask("Enter the option number:")
+  choice = gets.chomp.to_i
 
-  case choice.to_i
+  case choice
   when 1
-    task = prompt.ask("Enter the task:")
+    print "Enter the task: "
+    task = gets.chomp
     tasks << task
     puts "Task added: #{task}"
   when 2
     display_tasks(tasks)
   when 3
     display_tasks(tasks)
-    task_number = prompt.ask("Enter the task number to mark as completed:", convert: :int)
+    print "Enter the task number to mark as completed: "
+    task_number = gets.chomp.to_i
     if task_number > 0 && task_number <= tasks.length
       tasks[task_number - 1] += " [completed]"
-      puts "Task marked as completed."
+      puts "Task marked as completed"
     else
       puts "Invalid task number."
     end
   when 4
     display_tasks(tasks)
-    task_number = prompt.ask("Enter the task number to remove:", convert: :int)
+    print "Enter the task number to remove: "
+    task_number = gets.chomp.to_i
     if task_number > 0 && task_number <= tasks.length
       tasks.delete_at(task_number - 1)
       puts "Task removed."
@@ -60,7 +60,7 @@ loop do
   end
 end
 
-# Save tasks to a file
+#saving the tasks to a file
 File.open("tasks.txt", "w") do |file|
   tasks.each do |task|
     file.puts task
